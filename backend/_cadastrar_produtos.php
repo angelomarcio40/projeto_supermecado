b512599f3507baa6734dbdc9744869def5c3ffd6
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-08-03 22:30:03 -0300

    Projeto_supermecado

---
+++ backend/_cadastrar_produtos.php
@@ -0,0 +1,53 @@
+<?php
+
+// Include da conexão com banco de dados
+include 'conexao.php';
+
+try{
+    // exibe as variaveis globais recebidas via POST
+    // echo "<pre>";
+    // var_dump($_POST);
+    // echo "</pre>";
+    
+    // variaveis que recebm os dados enviados via POST
+    $produto = $_POST['produto'];
+    $categoria = $_POST['categoria'];
+    $fabricante = $_POST['fabricante'];
+    $valor = $_POST['valor'];
+    $validade = $_POST['validade'];
+
+    // variavel que recebe a querry SQL que será executada no BD
+    $sql = "INSERT INTO
+                tb_viagens
+            (
+                `produto`,
+                `categoria`,
+                `fabricante`,
+                `valor`,
+                `validade`
+            )
+            VALUES
+            (
+                '$produto',
+                '$categoria',
+                '$fabricante',
+                '$valor',
+                '$validade'
+                )
+            ";
+
+// prepara a execução da querry SQL acima
+$comando = $con->prepare($sql);
+
+// executa  o comando com a querryno banco de dados
+$comando->execute();
+
+// exibe msg de sucesso ao inserir
+echo "Cadastro realizado com sucesso!";
+
+}catch(PDOException $erro){
+
+}
+
+// Final da conexão
+?>
