46308fdcba941dc6edba128310e69b02aa127b17
angelomarcio40 <101677257+angelomarcio40@users.noreply.github.com>
2022-08-09 22:18:18 -0300

    adicionado css

---
+++ listar_produtos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Produtos</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="container">
+        <h3>Listagem de Produtos</h3>
+        <div id="grid-produtos">
+            <figure class="figure-produtos">
+                <img class="img-produtos" src="img/viagem-faltando.png" alt="imagem do produto">
+                <figcaption class="figcaption-produtos">
+                    <h4>Produto</h4>
+                    <h5>Categoria</h5>
+                    <h5>Fabricante</h5>
+                    <h5>Valor</h5>
+                    <small>Validade</small>
+                    <button class="btn-comprar">Comprar</button>
+                </figcaption>
+            </figure>
+        </div>
+    </div>
+    
+</body>
+</html>
